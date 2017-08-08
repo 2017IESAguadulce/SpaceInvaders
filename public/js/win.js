374e0f03fd72befcda610e30d0488abfa0e80b45
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-08-08 12:14:08 +0200

    Reasignadas coordenadas en estado win

--- public/js/win.js
+++ public/js/win.js
@@ -7,11 +7,11 @@ var winState = {
     create: function() {
 		// Cargamos fondo y mostramos mensaje final agregando instrucciones para reiniciar el juego
 		game.skin = game.add.sprite(0, 0, 'skin' + game.skinSeleccionada);
-		game.mapaTitulo = game.add.bitmapText(50, 35, 'gem', '', 54);
+		game.mapaTitulo = game.add.bitmapText(100, 80, 'gem', '', 54);
 		this.mostrarLetraPorLetra(game.mapaTitulo, '¡Has Ganado!');
-        game.regresar = game.add.text(80, 200, 'Pulse "Volver" para regresar al menú', {font: '30px Arial', fill: 'white' });
+        game.regresar = game.add.text(100, 300, 'Pulse "Volver" para regresar al menú', {font: '30px Arial', fill: 'white' });
 		// Agregamos el botón volver y su manejador para controlar sus eventos
-		game.btnVolver = game.add.button(game.world.centerX + 100, 450, 'botonVolver', this.manejadorClickBotonVolver, this, 0, 1, 0);
+		game.btnVolver = game.add.button(game.world.width - 300, 575, 'botonVolver', this.manejadorClickBotonVolver, this, 0, 1, 0);
 		game.btnVolver.onInputOver.add(this.manejadorOverBoton, this);
 		// Iniciamos la carga de las estrellas en pantalla
 		this.cargarEstrellas();
